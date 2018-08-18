3ac7a545dad35f875ea0024e975367757367ee16
supenniku <supenniku2@gmail.com>
2018-08-18 20:13:55 +0900

    ScriptTest 課題

---
+++ Assets/Test1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // 要素数5の配列を初期化する
+        int[] points = new int[5];
+
+        // 配列の各要素に値を代入する
+        points [0] = 30;
+        points [1] = 20;
+        points [2] = 40;
+        points [3] = 10;
+        points [4] = 70;
+
+        // 配列の要素をすべて表示する
+        for (int i = 4; i > -1; i--) {
+            Debug.Log (points [i]);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
